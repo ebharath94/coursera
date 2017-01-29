547d1aa9dfd50e1585a0ffda03c805cc3b782151
ebharath94 <ebharath94@gmail.com>
2017-01-29 21:12:12 +0530

    mod-4 solution
    my module-4 solution

--- module4 solutions/easier/script.js
+++ module4 solutions/easier/script.js
@@ -1,4 +1,4 @@
-// ******************************* 
+(function (window) {
 // START HERE IF YOU WANT AN EASIER STARTING POINT FOR THIS ASSIGNMENT
 // *******************************
 //
@@ -33,7 +33,6 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
@@ -58,11 +57,10 @@ for (var i = 0; i < names.length; i++) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
+  if (firstLetter == 'j') {
+     window.byeSpeaker.speak(names[i]);
   } else {
-    helloSpeaker.speak(names[i]);
+     window.helloSpeaker.speak(names[i]);
   }
 }
-
-})();
+})(window);
